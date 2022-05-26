3d576a6c23c1a69f9b37c7162d52858361de4aaa
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-05-26 09:56:29 -0600

    assertEqual function created/Tested

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ?
+    console.log(`🟢Assertion Passed: "${actual}" === "${expected}"`) :
+    console.log(`🔴Assertion Failed: "${actual}" !== "${expected}'`);
+};
+console.log(assertEqual);
+
+//Test code
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, "yes");
+assertEqual("red", "blue");
+assertEqual(-2, -2);
+assertEqual(true, true);
+assertEqual("green", "green");
