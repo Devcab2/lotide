94ae6911b00616bb4bf09746a1a14486a32b327d
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-05-26 19:54:58 -0600

    head function

--- head.js
+++ head.js
@@ -7,6 +7,24 @@ const assertEqual = function(actual, expected) {
 };
 console.log(assertEqual);
 
+const head = function(arr) {
+  return arr[0];
+};
+
+
+
+
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head([10]), 10);
+
+
+
+
+
 //CREATE A FUNCTION HEAD WHICH RETURNS THE FIRST ITEM IN THE ARRAY
 
 //USE ASSERTEQUAL TO WRITE TEST CASES FOR VARIOUS SCENARIOS
@@ -17,13 +35,3 @@ console.log(assertEqual);
 
 // AN EMPTY ARRAY SHOULD YIELD UNDEFINED
 
-const head = require("lodash");
-
-let arr = [1, 2, 3, 4, 5];
-
-let header = head.head(arr);
-
-assertEqual([5,6,7]);
-assertEqual(["Hello", "Lighthouse", "Labs"]);
-
-console.log(head);
