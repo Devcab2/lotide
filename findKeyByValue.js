e9b18e6b35677308688634c9dac18b302805fa11
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-06-09 14:51:05 -0600

    implement function that looks at the values of an object and returns the corresponding key

---
+++ findKeyByValue.js
@@ -0,0 +1,41 @@
+// func takes obj, val
+
+const findKeyByValue = function(obj, val) {
+
+  // get key from obj
+
+  const getKey = Object.keys(obj);
+
+  // loop through obj keys
+
+  for (const key of getKey) {
+
+    // if obj key = val return key
+
+    if (obj[key] === val) {
+
+      return key;
+
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+
+  sciFi: "The Expanse",
+
+  comedy: "Brooklyn Nine-Nine",
+
+  drama:  "The Wire"
+};
+
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"),);
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse") ,);
+
+console.log(findKeyByValue(bestTVShowsByGenre, "SpongeBob"),);
+
+/*
+this function takes an object and looks at the value and returns the corresponding key
+*/
