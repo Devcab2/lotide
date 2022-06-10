77214a79a4f496a6ed1aa68cc3d46f9e5867972b
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-06-10 00:25:50 -0600

    Implement find key which looks at an object and a callback, when a truthy value is read it prints the key assoiated with that value, if the key doesnt exist or valiue is falsey function will return undefined

---
+++ findKey.js
@@ -0,0 +1,56 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function(obj, func) {
+
+  for (let [key, value] of Object.entries(obj)) {
+
+    if (func(value)) {
+
+      return key;
+
+    }
+
+  }
+
+};
+
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1),"Blue Hill"));
+
+// passed stars 1 = Blue Hill
+
+
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3),"Akaleri"));
+
+// stars: 3 = Akaleri  passed
+
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1),"Bob"));
+
+// undefined key doesnt exist
+
+/*
+this function (findKey) looks at an object and a callback, returns the first key for which the callback returns a truthy value
+no key returns undefined
+*/
