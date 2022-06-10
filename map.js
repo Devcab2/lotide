7adc353005ea3003a8fa930de216d3b9179d3623
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-06-09 23:06:57 -0600

    implement map function which takes an array and a callback to make a new array that executes a callback on each value in the array.

---
+++ map.js
@@ -0,0 +1,35 @@
+const map = (array, callback) => {
+  
+  let newArray = [];
+
+  //initialize new empty array
+
+  for (let item of array) {
+
+    newArray.push(callback(item));
+
+    // loop through array get result of callback on array and push it too newArr
+
+  }
+
+  return newArray;
+
+};
+
+const words = ["Lilly", "Stephanie", "Baby"];
+
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word);
+const results3 = map(words, word => word.length);
+
+// map can be used different ways shown above
+
+
+console.log(results1); // [ "L", "S", "B"]
+console.log(results2); // ["Lilly", "Stephanie", "Baby"]
+console.log(results3); // [5, 9, 4]
+
+// note that the callback is only defined when calling map not inside the function (anonymous)
+
+
