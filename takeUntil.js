98dc0958e1dfe2acdd6823d0730c40b2e386c725
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-06-09 23:35:14 -0600

    implement takeUntil that takes an array and uses a callback on it and returns a new array with the elements being chaged by the calback. it stops whenever a falsey value is located.

---
+++ takeUntil.js
@@ -0,0 +1,27 @@
+const takeUntil = function(array, callback) {
+  
+  const result = [];
+
+  for (let item of array) {
+
+    if (!callback(item)) {
+
+      // loop and check for falsey
+
+      result.push(item);
+
+    } else {
+
+      return result;
+    
+    }
+
+  }
+  return result;
+};
+
+const array1 = [1, 2, 3, -4, -5];
+const callback1 = takeUntil(array1, x => x < 0);
+
+console.log(callback1); // [1, 2, 3]
+
