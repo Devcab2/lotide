6311ac23ef79f22a60859dc58cbff746d630d36b
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-06-20 14:42:13 -0600

    created test folder to store tests for different functions. in this case (assertEqual.js)

--- assertEqual.js
+++ assertEqual.js
@@ -5,15 +5,4 @@ const assertEqual = function(actual, expected) {
 };
 console.log(assertEqual);
 
-//Test code
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, "yes");
-assertEqual("red", "blue");
-assertEqual(-2, -2);
-assertEqual(true, true);
-assertEqual("green", "green");
-
-
 module.exports = assertEqual;
